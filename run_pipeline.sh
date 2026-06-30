d9643ae1a3fd8bcbbfff969f1c0f7ed14e5582a9
faseeha2701 <faseehazahra181@gmail.com>
2026-06-30 09:22:16 +0500

    Fixed my shell script for aptainer

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,5 +1,9 @@
-#!/usr/bin/env bash
 set -euo pipefail
 
-# Convenience wrapper for students (edit paths as needed)
-nextflow run main.nf -profile singularity   --reference data/reference.fasta   --reads data/sample.fastq.gz   --model_path /shared/clair3_models/hifi   --sample sample1   --outdir results   --threads 8
+nextflow run main.nf -profile apptainer \
+    --reference data/reference.fasta \
+    --reads data/sample.fastq \
+    --model_path /root/nextflow_hg/nextflow/models/hifi_sequel2 \
+    --sample sample1 \
+    --outdir results \
+    --threads 4
